ef4081f747d8a5b3169b5672187aacd14ae9d7f8
Arian Shaikh <ariansk90@gmail.com>
2022-07-23 01:08:43 +0530

    10x10 boxes

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -3,11 +3,7 @@
 
 //box colouring
 box_array = document.getElementsByClassName("box");
-for(let i = 0; i < box_array.length; i++){
-    if (i % 2 == 0){
-        box_array[i].classList.add("box_even");
-    }
-}
+
 
 // Logics
 
