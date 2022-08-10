5d24f749dde71733788b3496d4dcdf8339e9488b
TuhinBar <tuhinbr99@gmail.com>
2022-08-10 22:50:19 +0530

    Added one by one dice roll

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -11,125 +11,47 @@ var dice_p4 = document.getElementById("p4_roll");
 // console.log(dice_p1, dice_p2, dice_p3, dice_p4);
 
 //box colouring
-box_array = document.getElementsByClassName("box");
+let box_array = document.querySelectorAll(".box");
+// console.log(box_array)
 
 // Logics
 
 // ====Dice roll ====
+
+var turn = 1;
+
 // Player 1 move
-dice_p1.addEventListener("click", function() {
-    if(rollDice()==1){
-        p1_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
-        p1_roll.classList.add("bxs-dice-1");
-    }
-    else if(rollDice()==2){
-        p1_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
-        p1_roll.classList.add("bxs-dice-2");
-    }
-    else if(rollDice()==3){
-        p1_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
-        p1_roll.classList.add("bxs-dice-3");
-    }
-    else if(rollDice()==4){
-        p1_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
-        p1_roll.classList.add("bxs-dice-4");
-    }
-    else if(rollDice()==5){
-        p1_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
-        p1_roll.classList.add("bxs-dice-5");
-    }
-    else if(rollDice()==6){
-        p1_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
-        p1_roll.classList.add("bxs-dice-6");
+dice_p1.addEventListener("click", function(){
+    if(turn == 1){
+        player1Move();
+        turn = 2;
+        console.log("Player 1 rolled dice");
     }
 });
 // Player 2 move
-dice_p2.addEventListener("click", function() {
-
-    if(rollDice()==1){
-        p2_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
-        p2_roll.classList.add("bxs-dice-1");
-    }
-    else if(rollDice()==2){
-        p2_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
-        p2_roll.classList.add("bxs-dice-2");
-    }
-    else if(rollDice()==3){
-        p2_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
-        p2_roll.classList.add("bxs-dice-3");
-    }
-    else if(rollDice()==4){
-        p2_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
-        p2_roll.classList.add("bxs-dice-4");
-    }
-    else if(rollDice()==5){
-        p2_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
-        p2_roll.classList.add("bxs-dice-5");
-    }
-    else if(rollDice()==6){
-        p2_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
-        p2_roll.classList.add("bxs-dice-6");
+dice_p2.addEventListener("click", function(){
+    if(turn == 2){
+        player2Move();
+        turn = 3;
+        console.log("Player 2 rolled dice");
     }
 });
 // Player 3 move
-dice_p3.addEventListener("click", function() {
-
-    if(rollDice()==1){
-        p3_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
-        p3_roll.classList.add("bxs-dice-1");
-    }
-    else if(rollDice()==2){
-        p3_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
-        p3_roll.classList.add("bxs-dice-2");
-    }
-    else if(rollDice()==3){
-        p3_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
-        p3_roll.classList.add("bxs-dice-3");
-    }
-    else if(rollDice()==4){
-        p3_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
-        p3_roll.classList.add("bxs-dice-4");
-    }
-    else if(rollDice()==5){
-        p3_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
-        p3_roll.classList.add("bxs-dice-5");
-    }
-    else if(rollDice()==6){
-        p3_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
-        p3_roll.classList.add("bxs-dice-6");
+dice_p3.addEventListener("click", function(){
+    if(turn == 3){
+        player3Move();
+        turn = 4;
+        console.log("Player 3 rolled dice");
     }
 });
 // Player 4 move
-dice_p4.addEventListener("click", function() {
-    if(rollDice()==1){
-        p4_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
-        p4_roll.classList.add("bxs-dice-1");
-    }
-    else if(rollDice()==2){
-        p4_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
-        p4_roll.classList.add("bxs-dice-2");
-    }
-    else if(rollDice()==3){
-        p4_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
-        p4_roll.classList.add("bxs-dice-3");
-    }
-    else if(rollDice()==4){
-        p4_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
-        p4_roll.classList.add("bxs-dice-4");
-    }
-    else if(rollDice()==5){
-        p4_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
-        p4_roll.classList.add("bxs-dice-5");
-    }
-    else if(rollDice()==6){
-        p4_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
-        p4_roll.classList.add("bxs-dice-6");
+dice_p4.addEventListener("click", function(){
+    if(turn == 4){
+        player4Move();
+        turn = 1;
+        console.log("Player 4 rolled dice");
     }
 });
-
-
-
-
 
 
 // Funtions
@@ -138,3 +60,108 @@ function rollDice() {
     var dice = Math.floor(Math.random() * 6) + 1;
     return dice;
 }
+function player1Move(){
+        if(rollDice()==1){
+            p1_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
+            p1_roll.classList.add("bxs-dice-1");
+        }
+        else if(rollDice()==2){
+            p1_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
+            p1_roll.classList.add("bxs-dice-2");
+        }
+        else if(rollDice()==3){
+            p1_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
+            p1_roll.classList.add("bxs-dice-3");
+        }
+        else if(rollDice()==4){
+            p1_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
+            p1_roll.classList.add("bxs-dice-4");
+        }
+        else if(rollDice()==5){
+            p1_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
+            p1_roll.classList.add("bxs-dice-5");
+        }
+        else if(rollDice()==6){
+            p1_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
+            p1_roll.classList.add("bxs-dice-6");
+        }
+}
+function player2Move(){
+        if(rollDice()==1){
+            p2_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
+            p2_roll.classList.add("bxs-dice-1");
+        }
+        else if(rollDice()==2){
+            p2_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
+            p2_roll.classList.add("bxs-dice-2");
+        }
+        else if(rollDice()==3){
+            p2_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
+            p2_roll.classList.add("bxs-dice-3");
+        }
+        else if(rollDice()==4){
+            p2_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
+            p2_roll.classList.add("bxs-dice-4");
+        }
+        else if(rollDice()==5){
+            p2_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
+            p2_roll.classList.add("bxs-dice-5");
+        }
+        else if(rollDice()==6){
+            p2_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
+            p2_roll.classList.add("bxs-dice-6");
+        }
+}
+function player3Move(){
+
+        if(rollDice()==1){
+            p3_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
+            p3_roll.classList.add("bxs-dice-1");
+        }
+        else if(rollDice()==2){
+            p3_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
+            p3_roll.classList.add("bxs-dice-2");
+        }
+        else if(rollDice()==3){
+            p3_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
+            p3_roll.classList.add("bxs-dice-3");
+        }
+        else if(rollDice()==4){
+            p3_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
+            p3_roll.classList.add("bxs-dice-4");
+        }
+        else if(rollDice()==5){
+            p3_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
+            p3_roll.classList.add("bxs-dice-5");
+        }
+        else if(rollDice()==6){
+            p3_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
+            p3_roll.classList.add("bxs-dice-6");
+        }
+}
+function player4Move(){
+        if(rollDice()==1){
+            p4_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
+            p4_roll.classList.add("bxs-dice-1");
+        }
+        else if(rollDice()==2){
+            p4_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
+            p4_roll.classList.add("bxs-dice-2");
+        }
+        else if(rollDice()==3){
+            p4_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
+            p4_roll.classList.add("bxs-dice-3");
+        }
+        else if(rollDice()==4){
+            p4_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
+            p4_roll.classList.add("bxs-dice-4");
+        }
+        else if(rollDice()==5){
+            p4_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
+            p4_roll.classList.add("bxs-dice-5");
+        }
+        else if(rollDice()==6){
+            p4_roll.classList.remove("bxs-dice-1", "bxs-dice-2", "bxs-dice-3", "bxs-dice-4", "bxs-dice-5", "bxs-dice-6");
+            p4_roll.classList.add("bxs-dice-6");
+        }
+}    
